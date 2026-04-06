09cbd4e7c01e6cf6a4cce93906ec6666aa83862a
Steven <steven_891@icloud.com>
2026-04-06 23:53:47 +0800

    remove debug logging from release builds revert version to 0.0.1 for compatibility

--- RealDrawPileOrderCode/Patch.cs
+++ RealDrawPileOrderCode/Patch.cs
@@ -6,6 +6,7 @@ using MegaCrit.Sts2.Core.Nodes.Cards;
 using MegaCrit.Sts2.Core.Nodes.Screens;
 using MegaCrit.Sts2.Core.Nodes.Screens.CardLibrary;
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace RealDrawPileOrder.RealDrawPileOrderCode;
@@ -15,23 +16,29 @@ public static class TargetClass_MethodName_Patch {
 
     static readonly AccessTools.FieldRef<NCardPileScreen, NCardGrid> _gridRef = AccessTools.FieldRefAccess<NCardPileScreen, NCardGrid>("_grid");
 
-    private static void printCards(List<CardModel> cards) {
+    [Conditional("DEBUG")]
+    private static void printCards(CardPile pile) {
+        List<CardModel> cards = pile.Cards.ToList();
+        MainFile.Logger.Info("OnPileContentsChanged was called!");
+        MainFile.Logger.Debug("Pile type: " + pile.Type);
         MainFile.Logger.Debug("Cards[" + cards.Count + "]:");
         foreach (CardModel card in cards) {
             MainFile.Logger.Debug("Card: " + card.Id.Entry + ", Rarity: " + card.Rarity);
         }
     }
 
+
     static bool Prefix(NCardPileScreen __instance) {
-        MainFile.Logger.Info("OnPileContentsChanged was called!");
+        printCards(__instance.Pile);
+
         List<CardModel> list = __instance.Pile.Cards.ToList();
+        /*
+        // Removed the following if statement:
+        if (__instance.Pile.Type == PileType.Draw) {
+            list.Sort((CardModel c1, CardModel c2) => (c1.Rarity != c2.Rarity) ? c1.Rarity.CompareTo(c2.Rarity) : string.Compare(c1.Id.Entry, c2.Id.Entry, StringComparison.Ordinal));
+        } 
+        */
 
-        MainFile.Logger.Debug("Pile type: " + __instance.Pile.Type);
-        printCards(list);
-
-        // if (__instance.Pile.Type == PileType.Draw) {
-        //     list.Sort((CardModel c1, CardModel c2) => (c1.Rarity != c2.Rarity) ? c1.Rarity.CompareTo(c2.Rarity) : string.Compare(c1.Id.Entry, c2.Id.Entry, StringComparison.Ordinal));
-        // }
         NCardGrid grid = _gridRef(__instance);
         PileType type = __instance.Pile.Type;
         int num = 1;
@@ -41,7 +48,9 @@ public static class TargetClass_MethodName_Patch {
         int index = 0;
         span[index] = SortingOrders.Ascending;
         grid.SetCards(list, type, list2);
-        return false; // 返回 true -> 执行原方法；false -> 跳过
+
+        bool runOriginal = false;
+        return runOriginal;
     }
 
 }
